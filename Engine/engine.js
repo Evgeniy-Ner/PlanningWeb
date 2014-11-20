f9d178d45be9616958d97a526cbcdb94b854eb3e
Evgeniy N. <e.neronov@gmail.com>
2014-11-20 17:57:07 +0300

    InitialCommint

---
+++ Engine/engine.js
@@ -0,0 +1,64 @@
+﻿$(function () {
+
+
+
+    $.getJSON("GetObject.aspx?object=CalendarDays", function (data) {
+
+        //alert("Получен ответ :)");
+
+        var items = [];
+        var divs = [];
+
+        var color = 'green';
+
+        var green = 'green';
+        var blue = 'blue';
+
+        $.each(data, function (key, val) {
+
+
+            for(var i =0; i< 12; i++)
+            {
+                jQuery('<div/>', {
+                
+                    class: "div_hour_cell",
+                }).appendTo('#hourses_span');
+            }
+            
+
+
+        });
+
+        jQuery('<br/>', {
+
+        }).appendTo('#hourses_span');
+
+        $.each(data, function (key, val) {
+
+
+
+            jQuery('<div/>', {
+
+                class: "WorkShift",
+                text: val.descr
+            }).appendTo('#planning');
+
+
+        });
+
+
+        /*
+        $("<ul/>", {
+            "class": "my-new-list",
+            html: items.join("")
+        }).appendTo("body");
+
+        */
+
+
+
+    });
+
+
+
+});
---
+++ JSONProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace PlanningWebApp
+{
+    
+    public class JSONProvider
+    {
+        public static string GetOperations()
+        {
+            using (var cntx = new StoredProceduresDataContext())
+            {
+                return new JavaScriptSerializer().Serialize( cntx.P_PLN_WebGetOperatons() );   
+            }
+        }
+
+        public static string GetCalendarDays()
+        {
+            using (var cntx = new StoredProceduresDataContext())
+            {
+                return new JavaScriptSerializer().Serialize(cntx.PLN_WebGetDays());
+            }
+        }
+    }
+}
