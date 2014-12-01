4ac88bbc002bb1c35c4d07e1d3b2358488998606
Evgeniy N. <e.neronov@gmail.com>
2014-12-01 16:18:57 +0300

    cool timeline

---
+++ Engine/1.js
@@ -0,0 +1,183 @@
+﻿
+function ScaleTime(time) {
+    this.time = time;
+}
+
+ScaleTime.prototype.local = function () {
+    return new Date(this.time).toLocaleTimeString();
+}
+
+ScaleTime.prototype.date = function () {
+    return new Date(this.time).toLocaleDateString();
+}
+
+
+
+
+var OneSecond = 1000 * 60;
+var OneMinute = OneSecond * 60;
+var OneHour = OneMinute * 60;
+var OneDay = OneHour * 24;
+
+var scale = OneHour;
+
+var period = 0;
+
+var startTime;
+var endTime;
+
+var currentTime = new ScaleTime(new Date().getTime());
+
+var mainWidth;
+var startMilliSeconds;
+var endMilliSeconds;
+
+var startTime;
+var endTime;
+
+var DayPixels;
+var CountDays;
+
+var HourPixels;
+var CountHours;
+
+var MinutePixels;
+var CountMinutes;
+
+
+
+$(function () {
+
+    Init();
+
+});
+
+
+function Init() {
+
+    $("#main").empty();
+
+    $("#trace_scale").empty();
+
+    $(".day").empty();
+    $(".hour").empty();
+
+    $("#output").empty();
+
+    
+
+    $("#main").css("width", "1200px");
+
+    mainWidth = parseInt($(".main").css("width"));
+    
+    period = parseInt(scale * parseInt($(".main").css("width")) / 10);
+
+    startMilliSeconds = parseInt(currentTime.time) - period / 2;
+    endMilliSeconds = parseInt(currentTime.time) + period / 2;
+
+    startTime = new ScaleTime(startMilliSeconds);
+    endTime = new ScaleTime(endMilliSeconds);
+
+    CountDays = (endTime.time - startTime.time) / OneDay;
+
+    CountHours = (endTime.time - startTime.time) / OneHour;
+
+    CountMinutes = (endTime.time - startTime.time) / OneMinute;
+
+    CountSeconds = (endTime.time - startTime.time) / OneSecond;
+    
+    Draw("day", "main", 2560, CountDays);
+
+    Draw("hour", "day", 24, CountHours);
+
+    Draw("minute", "hour", 60, CountMinutes);
+
+    Draw("second", "minute", 60, CountSeconds);
+
+    TraceParams();
+
+
+   
+}
+
+
+function TraceParams()
+{
+    var where = "#output";
+
+    //$(where).empty();
+
+    $(where).append("<p> mainWidth: " + mainWidth + "</p>");
+    $(where).append("<p> period: " + period + "</p>");
+    $(where).append("<p> currentTime: " + currentTime.time + "</p>");
+
+    $(where).append("<p> startMilliSeconds: " + startMilliSeconds + "</p>");
+    
+    $(where).append("<p> startTime: " + startTime.date() + " "+ startTime.local() +  "</p>");
+    $(where).append("<p> endTime: " + endTime.date() + " " + endTime.local() + "</p>");
+
+    $(where).append("<p> CountDays: " + CountDays + "</p>");
+
+    $(where).append("<p> DayPixels: " + DayPixels + "</p>");
+
+    $(where).append("<p> CountHours: " + CountHours + "</p>");
+
+    $(where).append("<p> HourPixels: " + HourPixels + "</p>");
+
+    $(where).append("<p> CountMinutes: " + CountMinutes + "</p>");
+
+    $(where).append("<p> MinutePixels: " + MinutePixels + "</p>");
+
+    $(where).append("<p> scale: " + new Date(scale).getDate() + "days, " + new Date(scale).toLocaleTimeString() + "</p>");
+}
+
+
+
+$("html").keydown(function (event) {
+
+    if (event.which == 109) {
+        scale = scale + scale / 20;
+        Init();
+    }
+
+    if (event.which == 107) {
+        scale = scale - scale / 20;
+        Init();
+    }
+}
+ );
+
+
+
+
+function Draw(what_insert, where_insert, fullCount, placed)
+{
+    var pixels = mainWidth / placed;
+
+    var multipulir = Math.floor(10 / pixels + 1);
+
+    if (multipulir <= 12) {
+
+        $('#output').append("<p> multipulir: " + multipulir + "</p>");
+        $('#output').append("<p> width: " + pixels * 1 * multipulir + "</p>");
+
+        $("#output").append("" + what_insert + ": ");
+
+
+        var nCount = (placed > fullCount) ? fullCount : placed;
+
+        for (var i = 0; i < nCount / multipulir; i++) {
+            jQuery('<div/>', {
+
+                class: what_insert,
+
+                style: "left:" + pixels * i * multipulir + "px; ",
+
+                width: pixels * multipulir + "px"
+
+            }).appendTo("." + where_insert);
+
+            $("#output").append(i * multipulir + " ");
+        }
+    }
+}
---
+++ Engine/timelinecontrol.js
@@ -0,0 +1,185 @@
+﻿var size = 200;
+
+
+
+var scale = 1;
+
+var currentTime = new Date().getTime();
+var currentTimeFull = new Date(currentTime).toLocaleTimeString();
+
+var OneHour = 1000 * 60 * 60;
+
+var startTime = currentTime - OneHour * 24;
+
+function ScaleTime(time)
+{
+    this.time = time;
+}
+
+ScaleTime.prototype.local = function()
+{
+    return new Date(this.time).toLocaleTimeString();
+}
+
+var currentTime = new ScaleTime(new Date().getTime());
+
+
+var startTime = new ScaleTime(currentTime.time - OneHour * 12);
+var endTime = new ScaleTime(currentTime.time + OneHour * 12);
+
+
+
+
+
+$(function () {
+
+    Init();
+
+});
+
+
+$(window).resize(function () {
+    
+    Init();
+
+    
+
+});
+
+
+function Init()
+{
+
+    $(".timeline").empty();
+
+    $(".hour").empty();
+
+    $(".minute").empty();
+
+
+    $("#output").text("");
+
+    $("#timeline_time").text(new Date().toLocaleTimeString());
+
+    
+    var containerWidth = $(".container").css("width");
+
+    var count = Math.floor(parseInt(containerWidth) / size);
+
+    $("#output").append(currentTime.local() + " - " + currentTime.time);
+    $("#output").append("<br />");
+    $("#output").append(startTime.local() + " - " + startTime.time);
+    $("#output").append("<br />");
+    $("#output").append(endTime.local() + " - " + endTime.time);
+    $("#output").append("<br />");
+    $("#output").append("width: " + containerWidth);
+
+    $("#output").append("<br />");
+    $("#output").append("count: " + count);
+    $("#output").append("<br />");
+    $("#output").append("size: " + size);
+    $("#output").append("<br />");
+    $("#output").append("width  size =  " + (parseInt(containerWidth) / size));
+
+
+    $(".timeline").css("width", containerWidth);
+
+    $(".timeline").text("");
+
+    DrawHoursMarks();
+    DrawMinutesMarks();
+
+    if( (size / 60 / 6) > 5)
+        DrawSecondsMarks();
+
+
+    jQuery("<div />", { class: "minutes", text: "1" }).appendTo(".hour");
+
+}
+
+function DrawHoursMarks()
+{
+    var containerWidth = $(".container").css("width");
+
+    var count = Math.floor(parseInt(containerWidth) / size);
+
+    for (var i = 0; i < count; i++) {
+
+        jQuery('<div/>', {
+
+            class: "hour",
+
+            style: "left: " + i * size + "px",
+
+            width: size + "px"
+
+        }).appendTo(".timeline");
+
+    }
+
+    jQuery('<div/>', {
+
+        class: "hour",
+
+        style: "left: " + count * size + "px",
+
+        width: parseInt(containerWidth) % size + "px"
+
+    }).appendTo(".timeline");
+
+}
+
+function DrawMinutesMarks() {
+    
+    var minutesSize = size / 60;
+
+    for(var i = 0; i < 60; i++)
+    {
+
+        jQuery('<div/>', {
+
+            class: "minute",
+
+            style: "left: " + i * minutesSize + "px",
+
+            width: minutesSize + "px"
+
+        }).appendTo(".hour");
+    }
+
+}
+
+function DrawSecondsMarks() {
+
+    var minutesSize = size / 60 / 6;
+
+    for (var i = 0; i < 6; i++) {
+
+        jQuery('<div/>', {
+
+            class: "second",
+
+            style: "left: " + i * minutesSize + "px",
+
+            width: minutesSize + "px"
+
+        }).appendTo(".minute");
+    }
+
+}
+
+
+
+$("html").keydown(function (event) {
+
+    if (event.which == 109) {
+        size -= 50;
+        Init();
+    }
+
+    if (event.which == 107) {
+        size += 50;
+        Init();
+    }
+}
+ );
